1da91e1b68e114e1f67475f403dfcb8140d8759f
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-06-26 13:54:24 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from config import MONGO_URI, DB_NAME
+from models.db import init_db
+from routes.auth import auth_bp
+from routes.trade import trade_bp
+from routes.capital import capital_bp
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+
+# DB Setup
+init_db(MONGO_URI, DB_NAME)
+
+# Register Blueprints
+app.register_blueprint(auth_bp)
+app.register_blueprint(trade_bp)
+app.register_blueprint(capital_bp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
